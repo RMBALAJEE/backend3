de83b29e982dc6f960368dbabc7bedde1aa2c65e
BALAJEERM <balajee.rm@kluniversity.in>
2024-11-24 16:42:21 +0530

    fc

---
+++ src/main/java/com/klu/HeadphonesRepo.java
@@ -0,0 +1,7 @@
+package com.klu;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HeadphonesRepo extends JpaRepository<Headphones, Long> {
+	public Headphones findByPid(int pid);
+}
